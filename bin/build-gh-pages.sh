e8b874c14f0dc1912384ca3410a469a919867a19
Masayuki Uehara <munky69rock@gmail.com>
2020-08-22 14:37:33 +0900

    chore: tweaked

--- bin/build-gh-pages.sh
+++ bin/build-gh-pages.sh
@@ -4,6 +4,7 @@ set -ue
 
 root_dir=$(cd $(dirname $0)/..;pwd)
 sed=$(if (which gsed &> /dev/null); then echo gsed; else echo sed; fi)
+polyfill='<script src="https://cdn.jsdelivr.net/npm/@marp-team/marpit-svg-polyfill/lib/polyfill.browser.js"></script>'
 
 list_html() {
   find public -type f -name "*.html" | grep -v index.html
@@ -13,23 +14,35 @@ list_all() {
   find public -type f -name "*.pdf" -o -name "*.html"
 }
 
-cd "$root_dir"
-yarn marp
+replace_image_path() {
+  rsync -avzq images/ public/images/
+  $sed -i 's/"\.\//"..\//g' $(list_html)
+}
 
-# 画像のパスを差し替え
-rsync -avzq images/ public/images/
-$sed -i 's/"\.\//"..\//g' $(list_html)
+inject_polyfill() {
+  # iOS用のpolyfillをinject (https://github.com/marp-team/marpit-svg-polyfill)
+  $sed -i \
+    "s#</head>#${polyfill}</head>#" \
+    $(list_html)
+}
 
-# iOS用のpolyfillをinject (https://github.com/marp-team/marpit-svg-polyfill)
-$sed -i \
-  's#</head>#<script src="https://cdn.jsdelivr.net/npm/@marp-team/marpit-svg-polyfill/lib/polyfill.browser.js"></script></head>#' \
-  $(list_html)
+generate_index() {
+  slides=$(
+    list_all | \
+    $sed -e 's/public\///' -e 's/^/"/' -e 's/$/"/' | \
+    $sed -z -e 's/\n/,/g' -e 's/,$//'
+  ) 
+  data="{\"slides\": [${slides}]}"
+  yarn ejs -i "$data" index.ejs -o public/index.html
+}
 
-# index.htmlの作成
-slides=$(
-  list_all | \
-  $sed -e 's/public\///' -e 's/^/"/' -e 's/$/"/' | \
-  $sed -z -e 's/\n/,/g' -e 's/,$//'
-) 
-data="{\"slides\": [${slides}]}"
-npx ejs -i "$data" index.ejs -o public/index.html
+main() {
+  cd "$root_dir"
+
+  yarn marp
+  replace_image_path
+  inject_polyfill
+  generate_index
+}
+
+main

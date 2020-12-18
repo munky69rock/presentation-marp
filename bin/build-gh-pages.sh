47160d86750f485797ad874c4a9ee8ccdaf678ab
Masayuki Uehara <munky69rock@gmail.com>
2020-12-18 10:39:28 +0900

    feat: ready for presentation

--- bin/build-gh-pages.sh
+++ bin/build-gh-pages.sh
@@ -4,7 +4,6 @@ set -ue
 
 root_dir=$(cd $(dirname $0)/..;pwd)
 sed=$(if (which gsed &> /dev/null); then echo gsed; else echo sed; fi)
-polyfill='<script src="https://cdn.jsdelivr.net/npm/@marp-team/marpit-svg-polyfill/lib/polyfill.browser.js"></script>'
 
 list_html() {
   find public -type f -name "*.html" | grep -v index.html
@@ -19,20 +18,13 @@ replace_image_path() {
   $sed -i 's/"\.\//"..\//g' $(list_html)
 }
 
-inject_polyfill() {
-  # iOS用のpolyfillをinject (https://github.com/marp-team/marpit-svg-polyfill)
-  $sed -i \
-    "s#</head>#${polyfill}</head>#" \
-    $(list_html)
-}
-
 generate_index() {
   slides=$(
     list_all | \
     $sed -e 's/public\///' -e 's/^/"/' -e 's/$/"/' | \
     $sed -z -e 's/\n/,/g' -e 's/,$//'
   ) 
-  data="{\"slides\": [${slides}]}"
+  data="{\"slides\":[${slides}]}"
   yarn ejs -i "$data" index.ejs -o public/index.html
 }
 
@@ -41,7 +33,6 @@ main() {
 
   yarn marp
   replace_image_path
-  inject_polyfill
   generate_index
 }
 
